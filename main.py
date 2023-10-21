69cd4b51c59861678c229bdf70450ff07285f24d
Ed <edfry2918@gmail.com>
2023-10-21 17:23:55 +0100

    added map page with fake data, styling

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def get_home_page(request: Request):
     return templates.TemplateResponse("home_page.html", {"request": request})
+
+@app.get("/map", response_class=HTMLResponse)
+def get_map_page(request: Request):
+    return templates.TemplateResponse("map_page.html", {"request": request})
