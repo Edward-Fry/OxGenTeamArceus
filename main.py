0e003f33d82fe0bf3132b64f52aaa2aae533dc1f
Ed <edfry2918@gmail.com>
2023-10-21 19:06:27 +0100

    integrated first page with maps route

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from typing import Annotated
+
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import starlette.status as status
 
 app = FastAPI()
 
@@ -12,6 +15,10 @@ templates = Jinja2Templates(directory="templates")
 def get_home_page(request: Request):
     return templates.TemplateResponse("home_page.html", {"request": request})
 
-@app.get("/map", response_class=HTMLResponse)
-def get_map_page(request: Request):
-    return templates.TemplateResponse("map_page.html", {"request": request})
+@app.post("/", response_class=RedirectResponse)
+def get_home_page(prompt: Annotated[str, Form()]):
+    return RedirectResponse(f"/map/{prompt}", status_code=status.HTTP_302_FOUND)
+
+@app.get("/map/{prompt}", response_class=HTMLResponse)
+def get_map_page(request: Request, prompt: str):
+    return templates.TemplateResponse("map_page.html", {"request": request, "prompt": prompt})
