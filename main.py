33302b3c1654969bc40aa5b9cf8ef69450e553bf
Ed <edfry2918@gmail.com>
2023-10-22 00:29:57 +0100

    markers

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from typing import Annotated
 
 from fastapi import FastAPI, Request, Form
@@ -22,5 +23,8 @@ def get_home_page(prompt: Annotated[str, Form()]):
 
 @app.get("/map/{prompt}", response_class=HTMLResponse)
 def get_map_page(request: Request, prompt: str):
-    locations = gmaps.get_places_images_and_locations_from_text(prompt)
-    return templates.TemplateResponse("map_page.html", {"request": request, "prompt": prompt, "locations": locations})
+    # locations = gmaps.get_places_images_and_locations_from_text(prompt)
+    # json_locations = json.dumps(locations)
+    with open("mock_locations.json", "r") as file:
+        json_locations = file.read()
+    return templates.TemplateResponse("map_page.html", {"request": request, "prompt": prompt, "locations": json_locations})
